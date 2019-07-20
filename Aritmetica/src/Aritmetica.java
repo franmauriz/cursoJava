791ee00c796b06ccf8768d2846e4c4eab3208f42
fran mauriz pereira <franmaurizpereira@gmail.com>
2019-07-20 08:17:08 +0200

    modificacion ejercicio Aritmetica y agregar ejercicio caja

--- Aritmetica/src/Aritmetica.java
+++ Aritmetica/src/Aritmetica.java
@@ -1,7 +1,30 @@
 public class Aritmetica {
 
-    public int sumar(int a, int b){
-        int resultado = a +b;
-        return resultado;
+    int a;
+    int b;
+
+    Aritmetica(){
+
+    }
+
+    Aritmetica(int a,int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sumar(){
+        return (a + b);
+    }
+
+    int restar(){
+        return a-b;
+    }
+
+    int multiplica(){
+        return a*b;
+    }
+
+    int dividir(){
+        return a/b;
     }
 }
--- Aritmetica/src/Main.java
+++ Aritmetica/src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String args []){
-        //creamos un objeto de la clase Aitmetica
-        Aritmetica a = new Aritmetica();
-        //llamamos al metodo sumar
-        int resultado = a.sumar(4,10);
-        System.out.println(resultado);
+        // variables locales
+        int operandoA=6;
+        int operandoB=2;
+        //creamo bjeto Aritmetica con el constructor
+        Aritmetica objeto1= new Aritmetica(operandoA,operandoB);
+
+        System.out.println("operandoA = " + operandoA);
+        System.out.println("operandoB = " + operandoB);
+
+        System.out.println("Resultado de la suma: " + objeto1.sumar());
+        System.out.println("Resultado de la resta: " + objeto1.restar());
+        System.out.println("Resultado de la multiplicar: " + objeto1.multiplica());
+        System.out.println("Resultado de la division: " + objeto1.dividir());
     }
 }
