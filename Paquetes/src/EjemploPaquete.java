bfd19c40139a9ab1b23d4c926fdcb4278320062f
francisco mauriz <franmaurizpereira@gmail.com>
2019-07-25 10:20:46 +0200

    ejercicios Paquetes

---
+++ Paquetes/src/EjemploPaquete.java
@@ -0,0 +1,9 @@
+import static mauriz.fran.Utileria.imprimir;
+
+public class EjemploPaquete {
+
+    public static void main(String[] args) {
+            imprimir("hola");
+    }
+
+}
---
+++ Paquetes/src/mauriz/fran/Utileria.java
@@ -0,0 +1,8 @@
+package mauriz.fran;
+
+public class Utileria {
+
+    public static void imprimir(String texto){
+        System.out.println("texto = " + texto);
+    }
+}
