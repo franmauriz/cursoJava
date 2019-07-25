063a4a461b5441bb793e57ac2b9eac3c3eb9be22
francisco mauriz <franmaurizpereira@gmail.com>
2019-07-25 09:29:38 +0200

    ejercicio sobre carga de metodos

---
+++ SobrecargaMetodos/src/Operaciones.java
@@ -0,0 +1,17 @@
+public class Operaciones {
+
+    public static int sumar(int a ,int b){
+        System.out.print("sumar(int a ,int b)");
+        return a+b;
+    }
+
+    public static double sumar(double a , double b){
+        System.out.print("sumar(double a , double b)");
+        return a+b;
+    }
+
+    public static double sumar(int a , double b){
+        System.out.print("sumar(int a , double b)");
+        return a+b;
+    }
+}
---
+++ SobrecargaMetodos/src/SobrecargaMetodos.java
@@ -0,0 +1,10 @@
+public class SobrecargaMetodos {
+    public static void main(String[] args) {
+
+        System.out.println(" " + Operaciones.sumar(3,5));
+
+        System.out.println(" " + Operaciones.sumar(4.5,3.5));
+
+        System.out.println(" " + Operaciones.sumar(4, 5.5));
+    }
+}
