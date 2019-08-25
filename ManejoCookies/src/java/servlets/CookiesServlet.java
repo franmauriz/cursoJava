cd6aa32181fe597aefcdc47d3cc04f155a23453a
francisco mauriz pereira <franmaurizpereira@gmail.com>
2019-08-25 21:51:23 +0200

    ejercicio con cookies

---
+++ ManejoCookies/src/java/servlets/CookiesServlet.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author franmp
+ */
+@WebServlet(name = "CookiesServlet", urlPatterns = {"/CookiesServlet"})
+public class CookiesServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        boolean nuevoUsuario = true;
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null){
+            for(Cookie c : cookies){
+                if(c.getName().equals("visitanteRecurrente") && c.getValue().equals("si")){
+                    nuevoUsuario = false;
+                    break;
+                }
+            }
+        }
+        String mensaje = null;
+        if(nuevoUsuario){
+            Cookie visitanteCookie = new Cookie("visitanteRecurrente","si");
+            response.addCookie(visitanteCookie);
+            mensaje = "Gracias por visitar nuestro sitio";
+        }else{
+            mensaje = "Gracias por visitar de nuevo nuestro sitio";
+        }
+        response.setContentType("text/html");
+        try(PrintWriter out = response.getWriter()){
+            out.println("Mensaje:" + mensaje);
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
