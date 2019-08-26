b94437f882229e399669c64b4394912fddd2ca6e
francisco mauriz pereira <franmaurizpereira@gmail.com>
2019-08-26 22:03:35 +0200

    ejercicio cookies

---
+++ CookiesContador/src/java/servlets/ContadorServlet.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author franmp
+ */
+@WebServlet(name = "ContadorServlet", urlPatterns = {"/ContadorServlet"})
+public class ContadorServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        int contador =0;
+        Cookie[] cookies = request.getCookies();            
+        if(cookies!=null){
+            for(Cookie c : cookies){
+                try{
+                     if(c.getName().equals("contadorVisita")){
+                         contador = Integer.parseInt(c.getValue());
+                     }
+                }catch(NumberFormatException e){
+                    contador=0;
+                }
+            }
+        }
+        contador++;
+        Cookie c = new Cookie("contadorVisita",Integer.toString(contador));
+        c.setMaxAge(3600);
+        try (PrintWriter out = response.getWriter()) {
+           response.addCookie(c);
+           out.println("Contador de Visitas:" + contador);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
