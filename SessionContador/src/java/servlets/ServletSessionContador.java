b3a9a7d95ad44747a0d948b267972ed175683921
francisco mauriz pereira <franmaurizpereira@gmail.com>
2019-08-30 11:28:13 +0200

    ejercicio contador session

---
+++ SessionContador/src/java/servlets/ServletSessionContador.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ *
+ * @author ptl28sev000
+ */
+@WebServlet(name = "ServletSessionContador", urlPatterns = {"/ServletSessionContador"})
+public class ServletSessionContador extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession sesion = request.getSession();
+        
+        String titulo="";
+        Integer contadorVisitas = (Integer) sesion.getAttribute("contadorVisitas");
+        
+        if(contadorVisitas == null){
+            contadorVisitas = new Integer(1);
+            titulo="Bienvemido por primera vez ...";
+        }else{
+            titulo="Bienvenido de nuevo";
+            contadorVisitas++;
+        }
+        
+        sesion.setAttribute("contadorVisitas",contadorVisitas);
+        
+        try(PrintWriter out = response.getWriter()){
+            out.println("<h2>" + titulo + "</h2>");
+            out.println("Contador Visitas: " + contadorVisitas);
+            out.println("<br/>");
+            out.println("Id Session:" + sesion.getId());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
