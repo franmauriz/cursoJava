0b6777957106f539199c31de68f93fb28b9636cb
francisco mauriz pereira <franmaurizpereira@gmail.com>
2019-08-30 20:18:17 +0200

    Ejemplo carro compra session

---
+++ CarroCompra/src/java/servlets/ServletCompra.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author franmp
+ */
+@WebServlet(name = "ServletCompra", urlPatterns = {"/ServletCompra"})
+public class ServletCompra extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String articuloNuevo = request.getParameter("articulo");
+        HttpSession sesion = request.getSession();
+        
+        List<String> articulos = (List<String>) sesion.getAttribute("articulos");
+        
+        if(articulos==null){
+            articulos = new ArrayList<>();
+            sesion.setAttribute("articulos", articulos);
+        }
+        
+        if(articuloNuevo != null && !articuloNuevo.trim().equals("")){
+            articulos.add(articuloNuevo);
+        }
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletCompra</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1> Lista articulos</h1>");
+            out.println("<ul>");
+            for(String articulo : articulos){
+                out.println("<li>" + articulo + "</li>");
+            }
+            out.println("</ul>");
+            out.println("<br>");
+            out.println("<a href='/CarroCompra'>Volver</a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
