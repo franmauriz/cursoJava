193c193ba26d50de55e90749122b9f1633b60609
franmaurizpereira <franmaurizpereira@gmail.com>
2019-10-05 18:30:44 +0200

    EventListener y ActionListener

---
+++ JSF/eventosJSF/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,98 @@
+
+package beans.model;
+
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Named
+@RequestScoped
+
+public class Candidato {
+    
+    private String nombre;
+    private String apellido;
+    private int salarioDeseado;
+    private Date fechaNacimiento;
+    private String codigoPostal;
+    private String colonia;
+    private String ciudad;
+    
+    
+    Logger log = LogManager.getRootLogger();
+    
+    public Candidato(){
+        log.info("Crando el objeto Candidato");
+        this.setFechaNacimiento(new Date());
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        log.info("Modificando el atributo nombre: " + this.nombre);
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+        log.info("Modificando el atributo apellido: " + this.apellido);
+    }
+
+    public int getSalarioDeseado() {
+        return salarioDeseado;
+    }
+
+    public void setSalarioDeseado(int salarioDeseado) {
+        this.salarioDeseado = salarioDeseado;
+        log.info("Modificando el atributo salarioDeseado: " + this.salarioDeseado);
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+        log.info("Modificando el atributo fechaNacimiento: " + this.fechaNacimiento);
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+        log.info("Modificando el atributo codigoPostal: " + this.codigoPostal);
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+        log.info("Modificando el atributo colonia: " + this.colonia);
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+        log.info("Modificando el atributo ciudad: " + this.ciudad);
+    }
+    
+    
+    
+    
+    
+}
